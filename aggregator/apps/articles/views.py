16dd7c1e73274336d5aa7e0f520df9b36411df91
butovichev <butovichev@yandex.ru>
2017-08-19 19:48:59 +0300

    update

--- aggregator/apps/articles/views.py
+++ aggregator/apps/articles/views.py
@@ -1,16 +1,21 @@
 from django.views.generic import View
 from django.shortcuts import render
-from django.utils import timezone
-from django.http import HttpResponse, Http404, HttpResponseForbidden
+# from django.utils import timezone
+# from django.http import HttpResponse, Http404, HttpResponseForbidden
 
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from .models import Article
 
 
-
-
-class Index(View):
-    template_name = 'index.html'
+class Articles(View):
+    template_name = 'articles.html'
 
     def get(self, request):
-        return HttpResponse("aggregator")
+        data = self.get_data(request.city)
+        return render(request, self.template_name, {'posts': data})
+
+    def get_data(city):
+        articles = Article.objects.filter(city__domain_name=city)
+        return articles
 
